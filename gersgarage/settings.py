a77a4fdb72669a6ab7fe168d30ef92239b242bb1
claugf <63432597+claugf@users.noreply.github.com>
2021-01-31 14:52:59 +0000

    css

--- gersgarage/settings.py
+++ gersgarage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's1ngo49zd526fm30hr)ao^tl&#lkh03mo+va^72w=iv+ht^c^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['gersgarage-2020085.herokuapp.com', '127.0.0.1']
 
